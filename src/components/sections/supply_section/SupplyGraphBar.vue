ef788a7b19c797436133cb6f0dae2b0fa72f1c74
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-06-20 10:52:13 -0400

    add supply graph animation

--- src/components/sections/supply_section/SupplyGraphBar.vue
+++ src/components/sections/supply_section/SupplyGraphBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="supply-bar">
-    <div class="supply-bar-graph" :style="{ height: amount*25 + 'px' }">
+    <div class="supply-bar-graph is-animated" :style="{ height: amount*25 + 'px' }">
       <div class="supply-bar-graph-year">{{ year }}</div>
     </div>
-    <div class="supply-bar-amount">{{ amount }}m</div>
+    <div class="supply-bar-amount is-animated">{{ amount }}m</div>
   </div>
 </template>
 
@@ -25,16 +25,35 @@ export default {
   .supply-bar-amount {
     font-weight: bold;
     color: var.$ascent-color;
+
+    &.active {
+      animation: supply-bar-amount-anim 3s;
+    }
+  }
+}
+
+@keyframes supply-bar-amount-anim {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
   }
 }
 
 .supply-bar-graph {
-  background-color: var.$dark-ascent-color;
+  background: linear-gradient(var.$dark-ascent-color, transparent);
   border-top-left-radius: 10px;
   border-top-right-radius: 10px;
   position: relative;
   display: flex;
   justify-content: center;
+
+  &.active {
+    animation: supply-bar-graph-anim 0.5s cubic-bezier(0.230, 1.000, 0.320, 1.000) forwards;
+    transform-origin: bottom;
+  }
 
   .supply-bar-graph-year {
     position: absolute;
@@ -46,11 +65,21 @@ export default {
   }
 
   &:hover {
-    background-color: var.$ascent-color;
+    background: var.$ascent-color;
 
     .supply-bar-graph-year {
       color: black;
     }
+  }
+}
+
+@keyframes supply-bar-graph-anim {
+  0% {
+    transform: scaleY(0);
+  }
+
+  100% {
+    transform: scaleY(100%);
   }
 }
 </style>
