1e4863b65703f68d998efe253908e1fd8cbf5df0
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-11-15 11:35:39 -0500

    fix search logic, center layout, use display grid, highlight active inputs

--- src/components/sections/tasks_section/TasksSection.vue
+++ src/components/sections/tasks_section/TasksSection.vue
@@ -4,20 +4,21 @@
       <h3>Development status of the XELIS project</h3>
       <input type="text" class="task-search" placeholder="Search" :value="search" @input="e => setSearch(e.target.value || null)">
       <div class="task-status-filter">
-        <button @click="setStatus(null)">All Status</button>
-        <button @click="setStatus('In Progress')">In Progress</button>
-        <button @click="setStatus('Planned')">Planned</button>
-        <button @click="setStatus('Completed')">Completed</button>
+        <button :class="{ 'active': !status}" @click="setStatus(null)">All Status</button>
+        <button :class="{ 'active': status === 'In Progress'}" @click="setStatus('In Progress')">In Progress</button>
+        <button :class="{ 'active': status === 'Planned'}" @click="setStatus('Planned')">Planned</button>
+        <button :class="{ 'active': status === 'Completed'}" @click="setStatus('Completed')">Completed</button>
       </div>
       <div class="task-year-filter">
-        <button @click="setYear(null)">All years</button>
-        <button @click="setYear('2024')">2024</button>
+        <button :class="{ 'active': !year}" @click="setYear(null)">All Years</button>
+        <button :class="{ 'active': year === '2024'}" @click="setYear('2024')">2024</button>
       </div>
       <div class="task-list">
         <div v-for="(task, index) in tasks" v-bind:key="index">
           <TaskItem :name="task.name" :description="task.description" :status="task.status" :year="task.year" />
         </div>
       </div>
+      <div v-if="tasks.length === 0">No results</div>
       <div class="disclaimer">
         / Does NOT include previous achievements, internal tasks, bug fixes, maintenance, minor enhancements, etc.
       </div>
@@ -73,21 +74,23 @@ export default {
     },
     tasks() {
       return tasks.filter((task) => {
+        let filtered = true;
+
         if (this.status) {
-          return task.status === this.status;
+          filtered = task.status === this.status;
         }
 
-        if (this.year) {
-          return task.year === this.year;
+        if (this.year && filtered) {
+          filtered = task.year === this.year;
         }
 
-        if (this.search) {
+        if (this.search && filtered) {
           const name = task.name.toLowerCase();
           const searchValue = this.search.toLocaleLowerCase();
-          return name.indexOf(searchValue) !== -1;
+          filtered = name.indexOf(searchValue) !== -1;
         }
 
-        return true;
+        return filtered;
       });
     },
   }
@@ -100,27 +103,45 @@ export default {
 #tasks {
   position: relative;
   z-index: 1;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 
   h2 {
     margin-bottom: 0;
   }
 
+  h3 {
+    margin-bottom: 5rem;
+    margin-top: 1rem;
+    max-width: 40rem;
+    text-align: center;
+  }
+
   .task-list {
-    display: flex;
-    gap: 2.5rem;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+    gap: 3rem;
+    justify-items: center;
+    width: 100%;
   }
 
   .task-search {
     background: transparent;
-    border: .3rem solid white;
+    border: .2rem solid white;
     border-radius: 1rem;
     padding: 1.5rem 2.5rem;
     font-weight: bold;
     color: white;
     outline: none;
-    margin-bottom: 1rem;
+    margin-bottom: 2rem;
     width: 100%;
+    max-width: 100rem;
+    opacity: .5;
+  }
+
+  .task-search:active, .task-search:focus, .task-search:not(:placeholder-shown) {
+    opacity: 1;
   }
 
   .task-status-filter {
@@ -130,9 +151,23 @@ export default {
   }
 
   .task-year-filter {
-    margin-bottom: 2rem;
+    margin-bottom: 4rem;
     display: flex;
     gap: 1rem;
+  }
+
+  .task-status-filter, .task-year-filter {
+    button {
+      background: white;
+      border: none;
+      padding: .6rem 1rem;
+      opacity: .4;
+      cursor: pointer;
+    }
+
+    button:hover, button.active {
+      opacity: 1;
+    }
   }
 
   .disclaimer {
