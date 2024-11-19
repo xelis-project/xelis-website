933adb0f943ce91548a6986da49453c008172439
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-11-19 10:49:38 -0500

    better quad animation

--- src/components/sections/QuadSection.vue
+++ src/components/sections/QuadSection.vue
@@ -12,19 +12,19 @@
         <path d="M240.5 413.627L218.89 0L185.214 80.3017L240.5 413.627Z" fill="#02FFCF" fill-opacity="0.29"/>
         <path d="M262.235 0.125998L240.5 413.627L296.094 82.5503L262.235 0.125998Z" fill="#02FFCF" fill-opacity="0.29"/>
       </g>
-      <g class="privacy is-animated">
+      <g class="block privacy is-animated">
         <rect x="123.066" y="116" width="150" height="150" rx="10" transform="rotate(45 123.066 116)" fill="#02FFCF"/>
         <text fill="#094237" style="white-space: pre" font-family="Jura" font-size="16" font-weight="bold" letter-spacing="0em"><tspan x="90" y="228.476">PRIVACY</tspan></text>
       </g>
-      <g class="security is-animated">
+      <g class="block security is-animated">
         <rect x="240.066" y="1" width="150" height="150" rx="10" transform="rotate(45 240.066 1)" fill="#02FFCF"/>
         <text fill="#094237" style="white-space: pre" font-family="Jura" font-size="16" font-weight="bold" letter-spacing="0em"><tspan x="200" y="113.476">SECURITY</tspan></text>
       </g>
-      <g class="scalability is-animated">
+      <g class="block scalability is-animated">
         <rect x="355.066" y="116" width="150" height="150" rx="10" transform="rotate(45 355.066 116)" fill="#02FFCF"/>
         <text fill="#094237" style="white-space: pre" font-family="Jura" font-size="16" font-weight="bold" letter-spacing="0em"><tspan x="304" y="228.476">SCALABILITY</tspan></text>
       </g>
-      <g class="decentralization is-animated">
+      <g class="block decentralization is-animated">
         <rect x="240.066" y="230" width="150" height="150" rx="10" transform="rotate(45 240.066 230)" fill="#02FFCF"/>
         <text fill="#094237" style="white-space: pre" font-family="Jura" font-size="16" font-weight="bold" letter-spacing="0em"><tspan x="164" y="343.476">DECENTRALIZATION</tspan></text>
       </g>
@@ -65,49 +65,49 @@
 
   .quadlemma {
     animation: sideways 3s infinite alternate ease-in-out;
-    transform-origin: center center;
-    animation-delay: 2s;
+    transform-origin: center;
+    animation-delay: 1s;
 
-    .logo.active {
-      animation: down .5s forwards ease-out;
+    .logo {
       opacity: 0;
       transform: translate3d(0, -500px, 0);
+
+      &.active {
+        animation: down .5s forwards ease-out;
+      }
+    }
+
+    .block {
+      opacity: 0;
+      transform: scale3d(.5, .5, 1);
+      transform-origin: center center;
     }
 
     .security.active {
-      opacity: 0;
-      animation: appear .2s forwards ease-in-out;
-      animation-delay: .5s;
-      transform: scale3d(.5, .5, 1);
-      transform-origin: center center;
+      animation: appear .3s forwards cubic-bezier(0.68, -0.55, 0.27, 1.55);
+      animation-delay: .3s;
     }
 
     .scalability.active {
-      opacity: 0;
-      animation: appear .2s forwards ease-in-out;
-      animation-delay: .7s;
-      transform: scale3d(.5, .5, 1);
-      transform-origin: center center;
+      animation: appear .3s forwards cubic-bezier(0.68, -0.55, 0.27, 1.55);
+      animation-delay: .4s;
     }
     
     .decentralization.active {
-      opacity: 0;
-      animation: appear .2s forwards ease-in-out;
-      animation-delay: .9s;
-      transform: scale3d(.5, .5, 1);
-      transform-origin: center center;
+      animation: appear .3s forwards cubic-bezier(0.68, -0.55, 0.27, 1.55);
+      animation-delay: .5s;
     }
 
     .privacy.active {
-      opacity: 0;
-      animation: appear .2s forwards ease-in-out;
-      animation-delay: 1.1s;
-      transform: scale3d(.5, .5, 1);
-      transform-origin: center center;
+      animation: appear .3s forwards cubic-bezier(0.68, -0.55, 0.27, 1.55);
+      animation-delay: .6s;
     }
   }
 
   @keyframes down {
+    50% {
+      transform: translate3d(0, 20px, 0);
+    }
     100% {
       opacity: 1;
       transform: translate3d(0, 0, 0);
@@ -115,6 +115,18 @@
   }
 
   @keyframes appear {
+    50% {
+      opacity: 1;
+      transform: scale3d(1.2, 1.2, 1);
+    }
+    70% {
+      opacity: 1;
+      transform: scale3d(.9, .9, 1);
+    }
+    85% {
+      opacity: 1;
+      transform: scale3d(1.05, 1.05, 1);
+    }
     100% {
       opacity: 1;
       transform: scale3d(1, 1, 1);
@@ -123,13 +135,13 @@
 
   @keyframes sideways {
     0% {
-      transform: translate3d(0px, 0, 0) rotate3d(1, 1, 1, 0deg);
+      transform: translate3d(0, 0, 0);
     }
     50% {
-      transform: translate3d(5px, 0, 0) rotate3d(1, 1, 1, 5deg);
+      transform: translate3d(0, 4px, 0);
     }
     100% {
-      transform: translate3d(-5px, 0, 0) rotate3d(1, 1, 1, -5deg);
+      transform: translate3d(0, -4px, 0);
     }
   }
 }
