981697198989d66ade75a9d8f902566cb4fa160c
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-12-26 10:33:08 -0500

    add new exchanges (biconomy & safetrade)

--- src/components/sections/exchanges_section/exchanges.js
+++ src/components/sections/exchanges_section/exchanges.js
@@ -5,23 +5,23 @@ export default [
   {
     "name": "TradeOgre",
     "link": "https://tradeogre.com/exchange/XEL-USDT",
-    "logo_img": "https://docs.xelis.io/exchanges/tradeogre_logo.png",
-    "market_img": "https://docs.xelis.io/exchanges/tradeogre.png",
+    "logo_img": "https://docs.xelis.io/exchanges/tradeogre_logo.webp",
+    "market_img": "https://docs.xelis.io/exchanges/tradeogre.webp",
     "sort_volume": 3
   },
   {
     "name": "Exbitron",
     "link": "https://exbitron.com/trade?market=XEL-USDT",
-    "logo_img": "https://docs.xelis.io/exchanges/exbitron_logo.png",
-    "market_img": "https://docs.xelis.io/exchanges/exbitron.png",
-    "sort_volume": 6
+    "logo_img": "https://docs.xelis.io/exchanges/exbitron_logo.webp",
+    "market_img": "https://docs.xelis.io/exchanges/exbitron.webp",
+    "sort_volume": 7
   },
   {
     "name": "Seven Seas",
     "link": "https://www.sevenseas.exchange/market/XEL-USDT",
     "logo_img": "https://docs.xelis.io/exchanges/sevenseas_logo.webp",
-    "market_img": "https://docs.xelis.io/exchanges/sevenseas.png",
-    "sort_volume": 7
+    "market_img": "https://docs.xelis.io/exchanges/sevenseas.webp",
+    "sort_volume": 8
   },
   {
     "name": "NonKYC",
@@ -34,21 +34,35 @@ export default [
     "name": "Xeggex",
     "link": "https://xeggex.com/market/XEL_USDT",
     "logo_img": "https://docs.xelis.io/exchanges/xeggex_logo.webp",
-    "market_img": "https://docs.xelis.io/exchanges/xeggex.png",
+    "market_img": "https://docs.xelis.io/exchanges/xeggex.webp",
     "sort_volume": 4
   },
   {
     "name": "CoinEx",
     "link": "https://www.coinex.com/en/exchange/xel-usdt",
-    "logo_img": "https://docs.xelis.io/exchanges/coinex_logo.png",
-    "market_img": "https://docs.xelis.io/exchanges/coinex.png",
+    "logo_img": "https://docs.xelis.io/exchanges/coinex_logo.webp",
+    "market_img": "https://docs.xelis.io/exchanges/coinex.webp",
     "sort_volume": 2
   },
   {
     "name": "MEXC",
     "link": "https://www.mexc.com/exchange/XEL_USDT",
-    "logo_img": "https://docs.xelis.io/exchanges/mexc_logo.png",
-    "market_img": "https://docs.xelis.io/exchanges/mexc.png",
+    "logo_img": "https://docs.xelis.io/exchanges/mexc_logo.webp",
+    "market_img": "https://docs.xelis.io/exchanges/mexc.webp",
     "sort_volume": 1
+  },
+  {
+    "name": "Biconomy",
+    "link": "https://www.biconomy.com/exchange/XEL_USDT",
+    "logo_img": "https://docs.xelis.io/exchanges/biconomy_logo.webp",
+    "market_img": "https://docs.xelis.io/exchanges/biconomy.webp",
+    "sort_volume": 0
+  },
+  {
+    "name": "SafeTrade",
+    "link": "https://safe.trade/exchange/XEL-USDT",
+    "logo_img": "https://docs.xelis.io/exchanges/safetrade_logo.webp",
+    "market_img": "https://docs.xelis.io/exchanges/safetrade.webp",
+    "sort_volume": 6
   }
 ]
