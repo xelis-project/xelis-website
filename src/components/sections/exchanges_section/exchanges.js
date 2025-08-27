3d013345351e6315a33decb134a91333f1f91036
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-08-27 10:32:10 -0400

    remove exchanges

--- src/components/sections/exchanges_section/exchanges.js
+++ src/components/sections/exchanges_section/exchanges.js
@@ -2,13 +2,13 @@
 // use sort_volume to sort by volume
 
 export default [
-  {
+  /*{
     "name": "TradeOgre",
     "link": "https://tradeogre.com/exchange/XEL-USDT",
     "logo_img": "https://docs.xelis.io/exchanges/tradeogre_logo.webp",
     "market_img": "https://docs.xelis.io/exchanges/tradeogre.webp",
     "sort_volume": 6
-  },
+  },*/
   {
     "name": "Exbitron",
     "link": "https://exbitron.com/trade?market=XEL-USDT",
@@ -16,13 +16,13 @@ export default [
     "market_img": "https://docs.xelis.io/exchanges/exbitron.webp",
     "sort_volume": 8
   },
-  {
+  /*{
     "name": "Seven Seas",
     "link": "https://www.sevenseas.exchange/market/XEL-USDT",
     "logo_img": "https://docs.xelis.io/exchanges/sevenseas_logo.webp",
     "market_img": "https://docs.xelis.io/exchanges/sevenseas.webp",
     "sort_volume": 9
-  },
+  },*/
   {
     "name": "NonKYC",
     "link": "https://nonkyc.io/market/XEL_USDT",
@@ -65,13 +65,13 @@ export default [
     "market_img": "https://docs.xelis.io/exchanges/bit.webp",
     "sort_volume": 4
   },
-  {
+  /*{
     "name": "XT",
     "link": "https://www.xt.com/en/trade/xel_usdt",
     "logo_img": "https://docs.xelis.io/exchanges/xt_logo.webp",
     "market_img": "https://docs.xelis.io/exchanges/xt.webp",
     "sort_volume": 2
-  },
+  },*/
   {
     "name": "Trocador",
     "link": "https://trocador.app/en/?ref=1JXff8w6Gx&ticker_to=xel",
